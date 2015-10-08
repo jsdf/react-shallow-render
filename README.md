a932d7b23a88d1373200a8bd248878f50c117bbc
Darío Javier Cravero <dario@uxtemple.com>
2015-10-08 19:28:21 -0300

    refactor: use react-addons-test-utils
    React 0.14 deprecated `react/addons` in favour of a more modular approach.
    `TestUtils` was turned into `react-addons-test-utils`. This commit uses
    that package instead.

--- README.md
+++ README.md
@@ -10,3 +10,13 @@ const rendered = shallowRender(<div className="test" />);
 assert.equal(rendered.type, 'div');
 assert.equal(rendered.props.className, 'test');
 ```
+
+Please note that as of React 0.14, [shallow rendering won't render
+primitives](https://github.com/facebook/react/issues/4721).
+[The good thing is that you don't need it](https://github.com/facebook/react/issues/4721#issuecomment-135923690)
+as you can just do:
+
+```
+var el = React.createElement('h1', {}, 'Hello');
+console.log(el);
+```
--- test/shallowRender.js
+++ test/shallowRender.js
@@ -2,9 +2,17 @@ var tap = require('tap');
 var React = require('react');
 var shallowRender = require('../');
 
+var type = 'div';
+var props = {className: 'test'};
+function Test(props) {
+  return React.createElement(type, props);
+}
+
 tap.test('it works', function (t) {
-  var type = 'div';
-  var props = {className: 'test'};
-  t.similar(shallowRender(React.createElement(type, props)), {type: type, props: props});
+  var $component = shallowRender(React.createElement(Test, props));
+
+  t.equals($component.type, type, 'matches the type');
+  t.deepEquals($component.props, props, 'matches the props');
+
   t.end();
 });
